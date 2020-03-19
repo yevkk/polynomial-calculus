0919f805ebaa7419b93a7ebf25cb9b2a03ec4de5
Yevgeniy Kutsenko <47559940+yevkk@users.noreply.github.com>
2020-03-19 20:59:01 +0200

    plan

--- src/Polynomial.hpp
+++ src/Polynomial.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <BigNum.hpp>
+#include <vector>
+
 namespace lab {
 
 /**
@@ -8,9 +11,43 @@ namespace lab {
 class Polynomial
 {
 public:
+    //Polynomial(const Polynomial& that) = default;
+
+    //explicit Polynomial(std::string_view pol_str);
+
+    //Polynomial() = default;
+
+    //Polynomial& operator=(const Polynomial& pol)
+
+    //friend bool operator==(const Polynomial& left, const Polynomial& right);
+    //friend bool operator!=(const Polynomial& left, const Polynomial& right);
+
+    //friend Polynomial operator+(const Polynomial& left, const Polynomial& right);
+    //friend Polynomial operator-(const Polynomial& left, const Polynomial& right);
+    //friend Polynomial operator*(const Polynomial& left, const Polynomial& right);
+    //friend Polynomial operator*(const Polynomial& left, const BigNum& right);
+    //friend Polynomial operator*(const Polynomial& left, int right);
+
+    //template<typename OStream>
+    //friend OStream& operator<<(OStream& os, const Polynomial& pol);
+
+    //template<typename IStream>
+    //friend IStream& operator>>(IStream& is, const Polynomial& pol);
+
+    /*
+     * @return the highest power of variable with non-zero coefficient
+     */
+    //int degree();
+
+    //friend void modify(Polynomial& pol, const BigNum& mod);
+
+    //add, subtract, multiply
 
 private:
+    BigNum _field_modulo;
 
+    // Array of polynomial's coefficients
+    std::vector<BigNum> _coefs;
 };
 
 } // namespace lab
