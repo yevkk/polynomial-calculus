313636ecea39d0f15ccf1bf73f4add784b024c7e
Yevgeniy Kutsenko <47559940+yevkk@users.noreply.github.com>
2020-03-21 15:16:06 +0200

    test files preparation

---
+++ tests/TestPolynomial.cpp
@@ -0,0 +1,13 @@
+#include "../src/Polynomial.hpp"
+
+#include <sstream>
+
+#include "catch.hpp"
+
+TEST_CASE("Polynomials test", "[Polynoimal]") {
+    using namespace lab;
+
+    SECTION("---") {
+
+    }
+}
