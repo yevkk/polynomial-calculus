5b35b9eb4561d950ee86ac0d49d2af0134db0268
starquell <illia.gide@gmail.com>
2020-04-08 21:23:02 +0300

    added travis file

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/ilya/projects/polynomial-calculus
+# Build directory: /home/ilya/projects/polynomial-calculus/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("tests")
---
+++ build/tests/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/ilya/projects/polynomial-calculus/tests
+# Build directory: /home/ilya/projects/polynomial-calculus/build/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(TestAll "/home/ilya/projects/polynomial-calculus/build/bin/tests")
