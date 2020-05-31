9c3df2c14fc3dda3058a8c0c917e3d9b57d2906b
Yevgeniy Kutsenko <47559940+yevkk@users.noreply.github.com>
2020-05-31 08:33:49 +0300

    files created

---
+++ src/PolynomialRing.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "Polynomial.hpp"
+
+namespace lab {
+
+class PolynomialRing {
+public:
+
+
+private:
+    uint64_t _p;
+};
+
+} // namespace lab
+
+#include "PolynomialRing.hxx"
---
+++ src/PolynomialRing.hxx
@@ -0,0 +1,4 @@
+namespace lab {
+
+
+} // namespace lab
