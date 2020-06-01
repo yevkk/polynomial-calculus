9aa1f9b8df111ef8b61f36b2e012930a8a3193f8
Yevgeniy Kutsenko <47559940+yevkk@users.noreply.github.com>
2020-06-02 00:52:28 +0300

    cleared main

--- main.cpp
+++ main.cpp
@@ -2,21 +2,7 @@
 #include <PolynomialRing.hpp>
 #include <PolynomialField.hpp>
 
-#include <iostream>
-
 int main() {
-    using namespace lab;
-
-    PolynomialField field{3, Polynomial{1, 0, 1}};
-
-    auto elements = field.elements();
-    for (size_t i = 0; i < elements.size(); i++) {
-        std::cout << elements[i] << ',';
-        for (size_t j = 0; j < elements.size(); j++) {
-            std::cout << field.multiply(elements[i], elements[j]) << ',';
-        }
-        std::cout << std::endl;
-    }
 
     return 0;
 }
