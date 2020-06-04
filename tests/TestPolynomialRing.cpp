44c1bc4e22374347ece903e412d685e2fa7f35ca
Oleksii <o_oleksyshyn@ukr.net>
2020-06-04 10:50:16 +0300

    set filter on amount of irreducible polynomials of given degree

--- tests/TestPolynomialRing.cpp
+++ tests/TestPolynomialRing.cpp
@@ -463,10 +463,10 @@ TEST_CASE("Polynomial Rings test", "[Polynomial ring]") {
 
 
     SECTION("Integer number factorization") {
-        REQUIRE(detail::integerFactorization(24) == std::vector<uint64_t>{1, 24, 2, 12, 3, 8, 4, 6});
+        REQUIRE(detail::integerFactorization(24) == std::vector<uint64_t>{1, 2, 3, 4, 6, 8, 12, 24});
         REQUIRE(detail::integerFactorization(101) == std::vector<uint64_t>{1, 101});
-        REQUIRE(detail::integerFactorization(25) == std::vector<uint64_t>{1, 25, 5});
-        REQUIRE(detail::integerFactorization(256) == std::vector<uint64_t>{1, 256, 2, 128, 4, 64, 8, 32, 16});
+        REQUIRE(detail::integerFactorization(25) == std::vector<uint64_t>{1, 5, 25});
+        REQUIRE(detail::integerFactorization(256) == std::vector<uint64_t>{1, 2, 4, 8, 16, 32, 64, 128, 256});
     }
 
     SECTION("Irreducible polynomials of given order") {
@@ -474,29 +474,30 @@ TEST_CASE("Polynomial Rings test", "[Polynomial ring]") {
 
         REQUIRE(r3.irreducibleOfOrder(2) == std::vector{Polynomial{2, 1, 1}, Polynomial{2, 2, 1}});
         REQUIRE(r3.irreducibleOfOrder(3) == std::vector{
-                Polynomial{1, 2, 0, 1},
-                Polynomial{1, 2, 1, 1},
-                Polynomial{1, 1, 2, 1},
-                Polynomial{1, 0, 2, 1},
                 Polynomial{2, 2, 0, 1},
                 Polynomial{2, 1, 1, 1},
                 Polynomial{2, 0, 1, 1},
-                Polynomial{2, 2, 2, 1}
+                Polynomial{2, 2, 2, 1},
+                Polynomial{1, 2, 0, 1},
+                Polynomial{1, 2, 1, 1},
+                Polynomial{1, 1, 2, 1},
+                Polynomial{1, 0, 2, 1}
         });
+
 
         const PolynomialRing r5{5};
 
         REQUIRE(r5.irreducibleOfOrder(2) == std::vector<Polynomial>{
+                Polynomial{1, 1, 1},
+                Polynomial{1, 4, 1},
+                Polynomial{2, 0, 1},
+                Polynomial{3, 0, 1},
+                Polynomial{4, 2, 1},
+                Polynomial{4, 3, 1},
                 Polynomial{2, 1, 1},
                 Polynomial{3, 2, 1},
                 Polynomial{3, 3, 1},
                 Polynomial{2, 4, 1},
-                Polynomial{4, 2, 1},
-                Polynomial{4, 3, 1},
-                Polynomial{1, 1, 1},
-                Polynomial{2, 0, 1},
-                Polynomial{3, 0, 1},
-                Polynomial{1, 4, 1}
         });
 
         REQUIRE(r5.irreducibleOfOrder(4).size() == 150);
